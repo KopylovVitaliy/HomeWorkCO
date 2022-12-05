a40fde5b8fb6da9d5648e2c660296d519d602ddc
PioneerX7 <vita-kopy@yandex.ru>
2022-12-05 11:38:58 +0300

    начальный коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1 ();
+        task2 ();
+        task3 ();
     }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+        short age = 19;
+
+        if (age >= 18) {
+            System.out.println("Вам уже " + age + " лет. Вы достигли совершенолетия!");
+        } else {
+            System.out.println("Вам только " + age + " лет. Вы ещё не достигли совершенолетия, нужно немного подождать.");
+        }
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+        byte temp = 6;
+
+        if (temp < 5) {
+            System.out.println("На улице " + temp + " градусов, нужно надеть шапку.");
+        } else {
+            System.out.println("На улице " + temp + " градусов, можно идти без шапки.");
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+
+        short speed = 90;
+
+        if(speed > 60) {
+            System.out.println("Если скорость " + speed + " км/ч, то придётся заплатить штраф.");
+        } else {
+            System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно.");
+        }
+    }
+
 }
